2062a4bb5fa03642e3a911c5b9f14f314ba4ee02
unknown <lalab996@hotmail.com.tw>
2021-11-21 20:21:31 +0800

    version

--- src/App.vue
+++ src/App.vue
@@ -2,10 +2,21 @@
   <div id="app">
     <router-view :key="$route.fullPath" />
     <Rotate />
+    <div class="version">1.0</div>
   </div>
 </template>
 
 <style>
+.version {
+  position: fixed;
+  z-index: 10000;
+  right: 20px;
+  top: 20px;
+  color: green;
+  background-color: rgba(255, 255, 255, 0.8);
+  padding: 10px;
+  border-radius: 15px;
+}
 </style>
 
 <script>
